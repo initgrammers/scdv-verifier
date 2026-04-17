77161473c27bde635a8602dbc71d1ce29f96cd22
Cristian Ronda <cristian.ronda.dev@gmail.com>
2026-04-16 19:49:21 -0500

    config: enable vite-plugin-mkcert only in dev environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import mkcert from 'vite-plugin-mkcert';
+
+const isDev = process.env.NODE_ENV !== 'production';
 
 export default defineConfig({
   output: 'static',
@@ -8,5 +11,7 @@ export default defineConfig({
   integrations: [react(), tailwind()],
   vite: {
     optimizeDeps: { exclude: ['@noble/ed25519'] },
+    plugins: isDev ? [mkcert()] : [],
+    server: isDev ? { https: true } : {},
   },
 });
